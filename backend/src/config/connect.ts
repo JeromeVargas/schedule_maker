f99d5e26143cf92014603af40468997ba9c3af68
Jerome Vargas <jero124@yahoo.ca>
2023-08-23 10:54:43 -0500

    refactor: update mongoose

--- backend/src/config/connect.ts
+++ backend/src/config/connect.ts
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-// to prevent deprecated behavior
-mongoose.set("strictQuery", false);
-
 async function connectDB(): Promise<void> {
   const DB_URI = <string>process.env.MONGO_URI;
   await mongoose.connect(DB_URI);
