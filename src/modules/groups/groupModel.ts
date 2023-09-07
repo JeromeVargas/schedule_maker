86ab37fd69edfd935d483a5574a74ad234bf2709
Jerome Vargas <jero124@yahoo.ca>
2023-09-07 17:45:59 -0500

    feature: add group delete on cascade

--- src/modules/groups/groupModel.ts
+++ src/modules/groups/groupModel.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
-import { Group } from "../../typings/types";
+import { Group, Subject } from "../../typings/types";
+import ClassModel from "../classes/classModel";
+import SubjectModel from "../subjects/subjectModel";
 
 const GroupSchema = new Schema<Group>(
   {
@@ -29,6 +31,39 @@ const GroupSchema = new Schema<Group>(
   {
     timestamps: true,
     versionKey: false,
+  }
+);
+
+GroupSchema.pre(
+  "findOneAndDelete",
+  { document: false, query: true },
+  async function () {
+    /* get the entities ids and references */
+    // get the groups
+    const findGroup: Group | null = await this.model
+      // getFilter gets the parameters from the parent call, in this case findOneAndDelete
+      .findOne(this.getFilter(), { _id: 1, school_id: 1 })
+      .lean();
+    // get the subjects
+    const findSubjects: Subject[] = await SubjectModel.find({
+      school_id: findGroup?.school_id,
+      group_id: findGroup?._id,
+    })
+      .select("_id group_id")
+      .lean()
+      .exec();
+    /* delete entities records in collections */
+    // delete the subject instance/s
+    await SubjectModel.deleteMany({
+      school_id: findGroup?.school_id,
+      group_id: findGroup?._id,
+    }).exec();
+    // delete the class instance/s
+    await ClassModel.deleteMany({
+      subject_id: {
+        $in: findSubjects.map((subject) => subject._id),
+      },
+    });
   }
 );
 
