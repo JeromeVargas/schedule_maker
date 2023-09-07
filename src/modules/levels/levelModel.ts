fdf0ba50d0f83d52f136cd6800aaa8f8d7c62278
Jerome Vargas <jero124@yahoo.ca>
2023-09-07 16:46:48 -0500

    feature: add level delete on cascade

--- src/modules/levels/levelModel.ts
+++ src/modules/levels/levelModel.ts
@@ -1,5 +1,8 @@
 import { Schema, model } from "mongoose";
-import { Level } from "../../typings/types";
+import { Class, Group, Level, Subject } from "../../typings/types";
+import ClassModel from "../classes/classModel";
+import GroupModel from "../groups/groupModel";
+import SubjectModel from "../subjects/subjectModel";
 
 const LevelSchema = new Schema<Level>(
   {
@@ -20,6 +23,55 @@ const LevelSchema = new Schema<Level>(
   {
     timestamps: true,
     versionKey: false,
+  }
+);
+
+LevelSchema.pre(
+  "findOneAndDelete",
+  { document: false, query: true },
+  async function () {
+    /* get the entities ids and references */
+    // get the level
+    const findLevel: Level | null = await this.model
+      // getFilter gets the parameters from the parent call, in this case findOneAndDelete
+      .findOne(this.getFilter(), { _id: 1, school_id: 1 })
+      .lean();
+    // get the groups
+    const findGroups: Group[] = await GroupModel.find({
+      school_id: findLevel?.school_id,
+      level_id: findLevel?._id,
+    })
+      .select("_id level_id")
+      .lean()
+      .exec();
+    // get the subjects
+    const findSubjects: Subject[] = await SubjectModel.find({
+      school_id: findLevel?.school_id,
+      group_id: {
+        $in: findGroups.map((group) => group._id),
+      },
+    })
+      .select("_id group_id")
+      .lean()
+      .exec();
+    /* delete entities records in collections */
+    // delete the group instance/s
+    await GroupModel.deleteMany({
+      school_id: findLevel?.school_id,
+      level_id: findLevel?._id,
+    }).exec();
+    // delete the subject instance/s
+    await SubjectModel.deleteMany({
+      group_id: {
+        $in: findGroups.map((group) => group._id),
+      },
+    });
+    // delete the class instance/s
+    await ClassModel.deleteMany({
+      subject_id: {
+        $in: findSubjects.map((subject) => subject._id),
+      },
+    });
   }
 );
 
