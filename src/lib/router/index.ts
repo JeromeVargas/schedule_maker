805d615efffb6fa26a55937fa96a52d10caf546e
Jerome Vargas <jero124@yahoo.ca>
2024-11-02 11:36:05 -0500

    chore: change modules folder name for features

--- src/lib/router/index.ts
+++ src/lib/router/index.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { readdirSync } from "fs";
 import path from "path";
 
-const PATH_ROUTER = path.join("src", "modules");
+const PATH_ROUTER = path.join("src", "features");
 const router = Router();
 
 readdirSync(PATH_ROUTER).filter((folderName) => {
   console.log(`The /${folderName} route is being loaded`);
-  import(path.join("..", "..", "modules", `${folderName}`, "route")).then(
+  import(path.join("..", "..", "features", `${folderName}`, "route")).then(
     (moduleRouter) => {
       router.use(`/api/v1/${folderName}`, moduleRouter.router);
     }
