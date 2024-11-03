0492f8fd587081cb4d89e725796ca09b4716cf7b
Jerome Vargas <jero124@yahoo.ca>
2024-11-03 12:48:37 -0500

    style: move from end of the file to inline exports connection

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
-async function connectDB(): Promise<void> {
+export default async function connectDB(): Promise<void> {
   const DB_URI = <string>process.env.MONGO_URI;
   await mongoose.connect(DB_URI);
 }
-
-export default connectDB;
