0551883f61cfa17f0de6b39a7191e5c4e7894da5
Jerome Vargas <jero124@yahoo.ca>
2024-11-09 12:54:12 -0500

    refactor: add success false to errors

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -8,6 +8,7 @@ interface ExpressError extends Error {
   statusCode?: number;
   type?: string;
   errors?: ValidationError;
+  code?: number;
 }
 
 export default function errorHandlerMiddleware(
@@ -18,11 +19,15 @@ export default function errorHandlerMiddleware(
 ) {
   // The error is part of the set custom error
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+    return res
+      .status(err.statusCode)
+      .json({ msg: err.message, success: false });
   }
   // The error is part express-validator error
   if (err?.errors && Array.isArray(err.errors)) {
-    return res.status(StatusCodes.BAD_REQUEST).json(err?.errors);
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: err?.errors, success: false });
   }
   // The error is part of a JSON badly formatted req body
   if (
@@ -31,13 +36,15 @@ export default function errorHandlerMiddleware(
     err.type === "entity.parse.failed" &&
     "body" in err
   ) {
-    return res
-      .status(400)
-      .json({ msg: "Please make sure the submission is properly formatted" });
+    return res.status(400).json({
+      msg: "Please make sure the submission is properly formatted",
+      success: false,
+    });
   }
   // Fall out error
   return res.status(StatusCodes.CONFLICT).json({
     msg: "Something unexpected happened, please try again",
     info: process.env.NODE_ENV === "production" ? "Internal error" : err,
+    success: false,
   });
 }
